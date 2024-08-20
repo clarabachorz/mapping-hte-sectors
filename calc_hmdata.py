cf399f4599c2000a13c8f28df143463ade5f20c5
clarabachorz <clara.bachorz@pik-potsdam.de>
2024-08-20 11:49:00 +0200

    added supplementary fig: sensitivity of BF-BOF-CCS CAPEX

--- calc_hmdata.py
+++ calc_hmdata.py
@@ -52,6 +52,21 @@ def sup2_calc_dfs(param_set, param_dict):
 
     return [df_greenfield, df_brownfield, df_greenfieldcomp, df_brownfieldcomp]
 
+def sup3_calc_dfs(param_set, param_dict):
+    #bfccs sensitivity (+/- 50% on CCS capex)
+    temp_dict = dict(zip(param_dict.keys(), param_set))
+    load_json = True
+
+    df_normal = process_tech_df.get_df(scenario = "normal", load_json = load_json, **temp_dict)
+    df_lowCCS = process_tech_df.get_df(scenario = "lowCCS", load_json = load_json,ccs_steel_capex = 782.5, **temp_dict)
+    df_highCCS = process_tech_df.get_df(scenario = "highCCS", load_json = load_json,ccs_steel_capex = 978.7, **temp_dict)
+    df_normalcomp = process_tech_df.get_df(scenario = "normal_comp", DACCS=False, compensate=True, load_json = load_json, **temp_dict)
+    df_lowCCScomp = process_tech_df.get_df(scenario = "lowCCS_comp", DACCS=False, compensate=True, load_json = load_json,ccs_steel_capex = 782.5, **temp_dict)
+    df_highCCScomp = process_tech_df.get_df(scenario = "highCCS_comp", DACCS=False, compensate=True, load_json = load_json,ccs_steel_capex = 978.7, **temp_dict)
+    
+
+    return [df_normal, df_lowCCS, df_highCCS, df_normalcomp, df_lowCCScomp, df_highCCScomp]
+
 def save_mainfigdata(mainfig_list_of_dfs):
     list_of_dfs = [df for sublist in mainfig_list_of_dfs for df in sublist]  # Flatten the list
     df_final = pd.concat(list_of_dfs, ignore_index=True)
@@ -66,6 +81,11 @@ def save_sup2data(sup2_list_of_dfs):
     df_final = pd.concat(list_of_dfs, ignore_index=True)
     df_final.to_csv(str(Path(__file__).parent / 'data/supretrofit_rawdata.csv'))
 
+def save_sup3data(sup3_list_of_dfs):
+    list_of_dfs = [df for sublist in sup3_list_of_dfs for df in sublist]  # Flatten the list
+    df_final = pd.concat(list_of_dfs, ignore_index=True)
+    df_final.to_csv(str(Path(__file__).parent / 'data/supBFCCS_rawdata.csv'))
+
 if __name__ == "__main__":
     # calculates data for 3 heatmap figures: the main figure, supplementary figure with CO2 transport and storage sensitivity,
     # and supplementary with retrofit sensitivity
@@ -75,9 +95,11 @@ if __name__ == "__main__":
         mainfig_list_of_dfs = pool.starmap(mainfig_calc_dfs, [(param_set, mainfig_params()[0]) for param_set in mainfig_params()[1]])
         sup_list_of_dfs = pool.starmap(sup_calc_dfs, [(param_set, sup_params()[0]) for param_set in sup_params()[1]])
         sup2_list_of_dfs = pool.starmap(sup2_calc_dfs, [(param_set, mainfig_params()[0]) for param_set in mainfig_params()[1]])
+        sup3_list_of_dfs = pool.starmap(sup3_calc_dfs, [(param_set, mainfig_params()[0]) for param_set in mainfig_params()[1]])
 
     save_mainfigdata(mainfig_list_of_dfs)
     save_supdata(sup_list_of_dfs)
     save_sup2data(sup2_list_of_dfs)
+    save_sup3data(sup3_list_of_dfs)
 
 
